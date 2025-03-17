0e3d02358818abd53e905e4efb8d17470942da72
Vasileios Zisis <vasil.zisis@gmail.com>
2025-03-17 21:37:42 +0200

    added cluster and seeded db

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,9 @@ import mongoose from 'mongoose';
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(
-      'mongodb://localhost:27017/trackmyprogress'
-    );
+    const conn = await mongoose.connect(process.env.MONGO_URI);
     //process.env.MONGO_URI
+    //'mongodb://localhost:27017/trackmyprogress'
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.log(`Error: ${error.message}`);
